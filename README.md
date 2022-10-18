0e062d0e4891298f8561e8219fcd429943149312
Josh Toye <joshua.toye.developer@gmail.com>
2022-10-17 19:06:42 -0500

    Updated documentation and added __init__

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Pipeline CSV Converter
 
 This small python tool converts Harding Pipeline CSV files to Echo compliant CSV files.
+
+The tool expects a json file mapping subjects to departments and another file mapping instructor names to emails. These should be at ./subjToDept.json and ./nameToEmail.json respectively. Both in reference to the location of the convertPipelineToEcho.py.
+
+`usage: convertPipelineToEcho.py [-h] [-i INPUTPATH] [-o OUTPUTPATH] [-d] [-e SECONDINPUT]
+                                [-t TERM]
+
+options:
+  -h, --help      show this help message and exit
+  -i INPUTPATH    Path to input csv file.
+  -o OUTPUTPATH   Path to output converted file to.
+  -d              Only use this flag if you need to regenerate subjToDept.json
+  -e SECONDINPUT  Takes a second path to try and determine name/email mapping. Pass
+                  formatted file to i argument.
+  -t TERM         The term for the converted file. (e.g. Fall 2022)`
--- convertPipelineToEcho.py
+++ convertPipelineToEcho.py
@@ -27,6 +27,7 @@ def convertCSV(pathToFileToConvert, pathToOutput, subjToDept, nameToEmail, term)
             className = row["TITLE"]
             email = nameToEmail[row["INSTR"]] if row["INSTR"] in nameToEmail else "TBA"
             echoWriter.writerow([org, dept, cc, className, term, sect[1], email])
+        print(f"Output to: {pathToOutput}")
 
 def getDepartments(pathToFormattedCSV, pathToOutput):
     """Reads an Echo CSV and write a json file containing a mapping from subhect to department."""
@@ -87,9 +88,9 @@ def getEmails(formattedPath, unformattedPath, outputPath):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", dest="inputPath", help="Path to input csv file.")
-    parser.add_argument("-o", dest="outputPath", default="./subjToDept.json", help="Path to output converted file to.")
+    parser.add_argument("-o", dest="outputPath", default="./subjToDept.json", help="Path to output file.")
     parser.add_argument("-d", dest="getDepts", action="store_true", help="Only use this flag if you need to regenerate subjToDept.json")
-    parser.add_argument("-e", dest="secondInput", default=None, help="Takes a second path to try and determine name/email mapping. Pass formatted file to i argument.")
+    parser.add_argument("-e", dest="secondInput", default=None, help="Takes a second path to try and determine name/email mapping. Pass echo csv file to i argument.")
     parser.add_argument("-t", dest="term", help="The term for the converted file. (e.g. Fall 2022)")
 
     args = parser.parse_args()
